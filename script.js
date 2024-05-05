557c63f97be117e85a15ab4ba43fecd1d2f62ebc
AreeshaJat <areesha.jat2715@gmail.com>
2024-05-05 13:17:36 -0400

    finished console functionality

--- script.js
+++ script.js
@@ -16,42 +16,90 @@ const playerTwo = {
     symbol: "O"
 }
 
+let currentPlayer = playerOne;
+
+//row then column
 var winningCombinations = [
-    [1, 2, 3], [4, 5, 6], [7, 8, 9],
-    [1, 4, 7], [2, 5, 8], [3, 6, 9],
-    [1, 5, 9], [3, 5, 7]
+    [[0, 0], [0, 1], [0, 2]],   
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    [[0, 0], [1, 0], [2, 0]], 
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+    [[0, 0], [1, 1], [2, 2]],
+    [[0, 2], [1, 1], [2, 0]]
 ];
 
-let name1 = prompt("Enter a name for player 1");
-console.log(""+name1+" will be assigned the symbol X");
-let name2 = prompt("Enter a name for player 2");
-console.log(""+name2+" will be assigned the symbol O");
-drawBoard();
 //ask user for player 1
+playerOne.name = prompt("Enter a name for player 1");
 //ask user for player 2
+playerTwo.name = prompt("Enter a name for player 2");
+
 //start game
-//ask player 1 to pick an area on grid for x
-//ask player 2 to pick an area on grid for o
-//continue till someone wins
+while (true) {
+    drawBoard();
+    let player;
+    if (currentPlayer === playerOne) {
+        player = playerOne;
+    } else {
+        player = playerTwo;
+    }
+
+    let symbol = currentPlayer.symbol;
+    let playerName = currentPlayer.name;
+
+    //ask player 1 to pick an area on grid for x then
+    //ask player 2 to pick an area on grid for o
+    let move = prompt(""+playerName+" Enter a number between 1-9 to place your "+symbol+"");
+
+    //translates the user input string to int and then subtracts 1 since arrays are 0-8
+    let position = parseInt(move) - 1;
+
+    //checks if position variable is not a number, or less than 1, or greater than 9, or if the position is not already filled 
+    //Math.floor(position / 3) to get row index and position % 3 to get column index
+    if (isNaN(position) || position < 0 || position > 8 || Gameboard.gameboard[Math.floor(position / 3)][position % 3] !== "") {
+        console.log("Invalid move. Try again.");
+        continue;
+    }
+
+    //update gameboard
+    Gameboard.gameboard[Math.floor(position / 3)][position % 3] = symbol;
+
+    if(checkWin(currentPlayer)) {
+        drawBoard();
+        console.log(""+playerName+" won the game");
+        break;
+    }
+
+    if (tie()) {
+        drawBoard();
+        console.log("It's a tie");
+        break;
+    }
+
+    if (currentPlayer === playerOne) {
+        currentPlayer = playerTwo;
+    } else {
+        currentPlayer = playerOne;
+    }
+}
 
 function drawBoard () {
     console.log(" ");
-    console.log(' ' + (Gameboard.gameboard[0][0] || '1 ') + " | " + (Gameboard.gameboard[0][1] || ' 2 ') + " | " + (Gameboard.gameboard[0][2] || ' 3 '));
+    console.log(' ' + (Gameboard.gameboard[0][0] || ' ') + " | " + (Gameboard.gameboard[0][1] || '  ') + " | " + (Gameboard.gameboard[0][2] || '  '));
     console.log("---------------");
-    console.log(' ' + (Gameboard.gameboard[1][0] || '4 ') + " | " + (Gameboard.gameboard[1][1] || ' 5 ') + " | " + (Gameboard.gameboard[1][2] || ' 6 '));
+    console.log(' ' + (Gameboard.gameboard[1][0] || ' ') + " | " + (Gameboard.gameboard[1][1] || '  ') + " | " + (Gameboard.gameboard[1][2] || '  '));
     console.log("---------------");
-    console.log(' ' + (Gameboard.gameboard[2][0] || '7 ') + " | " + (Gameboard.gameboard[2][1] || ' 8 ') + " | " + (Gameboard.gameboard[1][2] || ' 9 '));
+    console.log(' ' + (Gameboard.gameboard[2][0] || ' ') + " | " + (Gameboard.gameboard[2][1] || '  ') + " | " + (Gameboard.gameboard[2][2] || '  '));
     console.log(" ");
 }
 
 //win function
-function checkWin(playerOne, playerTwo) {
+function checkWin(player) {
     for (let i = 0; i < winningCombinations.length; i++) {
         const [a, b, c] = winningCombinations[i];
-        if(Gameboard[a] === playerOne && Gameboard[b] === playerOne && Gameboard[c] === playerOne) {
-            return true
-        } else if (Gameboard[a] === playerTwo && Gameboard[b] === playerTwo && Gameboard[c] === playerTwo) {
-            return true
+        if(Gameboard.gameboard[a[0]][a[1]] === player.symbol && Gameboard.gameboard[b[0]][b[1]] === player.symbol && Gameboard.gameboard[c[0]][c[1]] === player.symbol) {
+            return true;
         }
     }
     return false;
@@ -59,9 +107,11 @@ function checkWin(playerOne, playerTwo) {
 
 //tie function
 function tie() {
-    for (let i = 0; i < Gameboard.length; i++) {
-        if (Gameboard[i] === '') {
-            return false;
+    for (let i = 0; i < Gameboard.gameboard.length; i++) {
+        for (let j = 0; j < Gameboard.gameboard.length; j++) {
+            if (Gameboard.gameboard[i][j] === '') {
+                return false;
+            }
         }
     }
     return true;
